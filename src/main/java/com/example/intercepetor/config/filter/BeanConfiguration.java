5dc0e5e51319a30cd7fc850f970d8e0a55e966e2
xueweidong <xueweidong@iboxpay.com>
2020-01-21 14:29:19 +0800

    解决跨域请求(后台其实能访问 只是浏览器将返回结果拦截了，现在去除这个限制)

---
+++ src/main/java/com/example/intercepetor/config/filter/BeanConfiguration.java
@@ -0,0 +1,33 @@
+package com.example.intercepetor.config.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @Author: XueWeiDong
+ * @Description:
+ * @Date: 14:17 2020/1/21
+ */
+@Configuration
+public class BeanConfiguration {
+
+    /**
+     * 跨域访问
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+
+}
